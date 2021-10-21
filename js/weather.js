ad0786ab330702d6997b4ad77085f88b1717f10d
XeNuM-dev <urbakh2013@mail.ru>
2021-10-21 19:29:53 +0300

    init demo

---
+++ js/weather.js
@@ -0,0 +1,120 @@
+const url = 'js/data.json'
+const temperatureUnit = '&deg'
+const humidityUnit = ' %' 
+const pressureUnit = ' мм. рт. ст.'
+const windUnit = ' м/с'
+
+let currentData;
+
+async function getData() {
+    let response = await fetch(url)
+
+    if (response.ok) {
+        let jsonData = response.json()
+        return jsonData
+    } else {
+        alert('Error: ' + response.status)
+    }
+}
+
+function convertPressure(value) {
+    return (value/1.33).toFixed()
+}
+
+Number.prototype.pad = function(size) {
+    let s = String(this)
+    while (s.length < (size || 2)) {s = "0" + s}
+    return s
+}
+
+function getHoursString(dateTime) {
+let date = new Date(dateTime)
+let hours = date.getHours().pad()
+
+return hours
+}
+
+function getValueWithUnit(value, unit) {
+    return `${value}${unit}`
+}
+
+function getTemperature(value) {
+    let roundedValue = value.toFixed()
+    return getValueWithUnit(roundedValue, temperatureUnit)
+}
+
+function render(data) {
+    renderCity(data)
+    renderCurrentDescription(data)
+    renderCurrentTemperature(data)
+    renderForecast(data)
+    renderDetails(data)
+}
+
+function renderCity(data) {
+    let cityName = document.querySelector('.current__city')
+    cityName.innerHTML = data.city.name
+}
+
+function renderCurrentTemperature(data) {
+    let tmp = data.list[0].main.temp
+
+    let currentTmp = document.querySelector('.current__temperature')
+    currentTmp.innerHTML = getTemperature(tmp)
+}
+
+function renderCurrentDescription(data) {
+    let tmp = data.list[0].weather[0].description
+
+    let description = document.querySelector('.current__description')
+    description.innerHTML = tmp
+}
+
+function renderForecast(data) {
+    let forecastDataContainer = document.querySelector('.forecast')
+    let forecasts = ''
+
+    for (let i = 0; i < 6; i++) {
+        let item = data.list[i]
+
+        let icon = item.weather[0].icon
+        let temp = getTemperature(item.main.temp)
+        let hours = ( i == 0 ? "Сейчас" : getHoursString(item.dt * 1000))
+
+        let template = `<div class="forecast__item">
+          <div class="forecast__time">${hours}</div>
+          <div class="forecast__icon icon__${icon}"></div>
+          <div class="forecast__temperature">${temp}</div>
+         </div> ` 
+         forecasts += template 
+    }
+    forecastDataContainer.innerHTML = forecasts
+}
+
+function renderDetails(data) {
+    let item = data.list[0]
+    let pressureValue = convertPressure(item.main.pressure)
+    let pressure = getValueWithUnit(pressureValue, pressureUnit)
+    let humidity = getValueWithUnit(item.main.humidity, humidityUnit)
+    let feels_like = getTemperature(item.main.feels_like)
+    let wind = getValueWithUnit(item.wind.speed, windUnit)
+
+    renderDetailsItem('feelslike', feels_like)
+    renderDetailsItem('humidity', humidity)
+    renderDetailsItem('pressure', pressure)
+    renderDetailsItem('wind', wind)
+}
+
+function renderDetailsItem(className, value) {
+    let container = document.querySelector(`${className}`).querySelector('details__value')
+    container.innerHTML = value
+}
+
+function start() {
+    getData().then(data => {
+        currentData = data
+        render(data)
+    })
+}
+
+start()
